f1f8dafb7dfbacaebefb10a70d6f9deeaecf73d1
Dre <montenegrodiussantos1@gmail.com>
2024-06-15 16:28:53 +0800

    Add Project

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
-import project1 from '../assets/projects/project-1.png';
-import project2 from '../assets/projects/project-2.png';
-import project3 from '../assets/projects/project-3.jpg';
-import project4 from '../assets/projects/project-4.jpg';
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `
 I'm a Frontend Developer focused on React, with exposure to Full Stack Development. I'm a passionate coder driven by tech innovation, a quick learner, and eager to tackle new challenges. My collaborative spirit makes me a valuable team member, always striving to enhance my skills and contribute to meaningful projects.`;
@@ -10,45 +10,45 @@ export const ABOUT_TEXT = `I am a versatile frontend developer passionate about 
 
 export const EXPERIENCES = [
     {
-        year: 'Apr 2023 - Dec 2023',
-        role: 'Customer Service Representative (CSR)',
-        company: 'Chirpish LLC.',
+        year: "Apr 2023 - Dec 2023",
+        role: "Customer Service Representative (CSR)",
+        company: "Chirpish LLC.",
         description: `Promoted as an email support (NON-VOICE) within 3 weeks. Awarded 2x Best in Customer Service, 4x Best in Email Support.`,
-        technologies: ['Shopify', 'Konnektive CRM', 'Dialpad'],
+        technologies: ["Shopify", "Konnektive CRM", "Dialpad"],
     },
     {
-        year: 'Oct 2022 - Apr 2023',
-        role: 'Website Management VA',
-        company: 'T&Co Virtual assistance',
+        year: "Oct 2022 - Apr 2023",
+        role: "Website Management VA",
+        company: "T&Co Virtual assistance",
         description: `Effectively enhanced the T&Co. website, resulting in a monthly average increase of 22 clients. Awarded as a Top Trainee and got promoted as Website management VA for the company operations.`,
-        technologies: ['Weebly', 'SEO', 'Google Analytics'],
+        technologies: ["Weebly", "SEO", "Google Analytics"],
     },
 ];
 
 export const PROJECTS = [
     {
-        title: 'Shady Shop: E-Commerce Website',
+        title: "Shady Shop: E-Commerce Website",
         image: project1,
-        link: 'https://youtu.be/al8bGhoxZIE',
+        link: "https://youtu.be/al8bGhoxZIE",
         description:
-            'Shady Shop: MERN Stack e-commerce with sleek UI, Express and MongoDB backend, and seamless PayPal checkout. Users browse, search, purchase; admins manage users, products.',
-        technologies: ['React', 'Redux', 'Node/Express', 'MongoDB'],
+            "Shady Shop: MERN Stack e-commerce with sleek UI, Express and MongoDB backend, and seamless PayPal checkout. Users browse, search, purchase; admins manage users, products.",
+        technologies: ["React", "Redux", "Node/Express", "MongoDB"],
     },
     {
-        title: 'Axiomcast: Video Conference App',
+        title: "Axiomcast: Video Conference App",
         image: project2,
-        link: 'https://axiomcast.vercel.app',
+        link: "https://axiomcast.vercel.app",
         description:
-            'AxiomCast: Empowering teams with cutting-edge virtual collaboration. Crystal-clear video conferencing, interactive screen sharing, and advanced meeting controls redefine teamwork.',
-        technologies: ['Nextjs', 'Typescript', 'Tailwind CSS'],
+            "AxiomCast: Empowering teams with cutting-edge virtual collaboration. Crystal-clear video conferencing, interactive screen sharing, and advanced meeting controls redefine teamwork.",
+        technologies: ["Nextjs", "Typescript", "Tailwind CSS"],
     },
     {
-        title: 'Product Dashboard',
+        title: "Product Dashboard",
         image: project4,
-        link: 'https://www.youtube.com/watch?v=WL8FgM3h_R0',
+        link: "https://www.youtube.com/watch?v=WL8FgM3h_R0",
         description:
-            'Product Dashboard is a CodeIgniter web app for managing products and user accounts, with admin controls, reviews, and MySQL data storage.',
-        technologies: ['PHP', 'CodeIgniter 3', 'WAMP', 'mySQL'],
+            "Product Dashboard is a CodeIgniter web app for managing products and user accounts, with admin controls, reviews, and MySQL data storage.",
+        technologies: ["PHP", "CodeIgniter 3", "WAMP", "mySQL"],
     },
     // {
     //     title: 'DsTube',
@@ -61,7 +61,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-    address: 'President Roxas, North Cotabato, Philippines, 9405',
-    phoneNo: '+63 970 460 7775',
-    email: 'montenegrodiussantos1@gmail.com',
+    address: "President Roxas, North Cotabato, Philippines, 9405",
+    phoneNo: "+63 970 460 7775",
+    email: "montenegrodiussantos1@gmail.com",
 };
